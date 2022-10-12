34b5fba3904bf2ed68eabc227d9736f781f26d50
stephen ligtenberg <bountyhunter24@gmail.com>
2022-10-11 22:38:01 -0400

    commit tree 1a890df5199d828cbfbb852b0ad9bb5b953c59a0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+// Add your code here  
+
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        }),
+    })
+    .then(response => response.json())
+    .then(object => document.body.textContent = object['id'])
+    .catch(error => document.body.textContent = (error.message));
+}
